809d9e01c562d0bd8ab2475b9bf4e2d2b94388ee
Alekanteri <sandor.basc@gmail.com>
2023-11-18 17:12:17 +0300

    fixes

--- src/components/Main.tsx
+++ src/components/Main.tsx
@@ -5,7 +5,7 @@ import VisualBlock from "./MainComponents/VisualBlock";
 const Main: React.FC = () => {
   return (
     <div className={styles.container}>
-        <div className={styles.visualBlockBody}>
+        <div className={styles.visualBody}>
           <VisualBlock className={styles.visualBlock}>
           Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
             molestiae, enim in ipsa hic eos magni repellat unde cupiditate
@@ -28,58 +28,14 @@ const Main: React.FC = () => {
             123
           </VisualBlock>
         </div>
-      <div className={styles.sheetBody}>
-        <div className={styles.sheetBlock}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?Lorem ipsum dolor sit amet consectetur adipisicing elit. Architecto
-            molestiae, enim in ipsa hic eos magni repellat unde cupiditate
-            necessitatibus recusandae assumenda omnis voluptatibus nulla tempore
-            nam consequatur, fugit quis?</div>
-        <div className={styles.sheetBlock}>dfjklasdlf;</div>
-      </div>
+        <div className={styles.cont}>
+          <div className={styles.right}>
+          Note that any other scrolls, such as those performed by the user, are not affected by this property. When this property is specified on the root element, it applies to the viewport instead. This property specified on the body element will not propagate to the viewport.
+          </div>
+          <div className={styles.left}>
+          Note that any other scrolls, such as those performed by the user, are not affected by this property. When this property is specified on the root element, it applies to the viewport instead. This property specified on the body element will not propagate to the viewport.
+          </div>
+        </div>
     </div>
   );
 };
